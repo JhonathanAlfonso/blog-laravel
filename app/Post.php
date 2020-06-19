bdaff90ea931ed3aaaefab05bdf938396911b2dc
salajhon69 <salajhon69@gmail.com>
2020-06-19 13:51:22 -0500

    feat(post): tag feature
    n-n migration was added in addition to Tag models, in the Post model the tags() function was created
    to access the tag attribute of Tag. Additionally, migration was added

--- app/Post.php
+++ app/Post.php
@@ -7,9 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model {
 
     protected $dates = ['published_at'];
+    /**
+     * @var mixed|string
+     */
 
     public function category() {
 
         return $this->belongsTo(Category::class);
+    }
+
+    public function tags() {
+
+        return $this->belongsToMany(Tag::class);
     }
 }
